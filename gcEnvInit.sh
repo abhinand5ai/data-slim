2eb4b36233530a9ef346da0084026ef6cfbd3e7b
abhinand <abhinand@tamu.edu>
2023-02-16 12:29:00 -0600

    envinit

--- gcEnvInit.sh
+++ gcEnvInit.sh
@@ -1,8 +1,10 @@
 
-source  /opt/gc/poplar/poplar_sdk-ubuntu_18_04-2.5.1+1001-64add8f33d/poplar-ubuntu_18_04-2.5.0+4748-e94d646535/enable.sh
 
+source /opt/gc/poplar/poplar_sdk-ubuntu_20_04-3.1.0+1205-58b501c780/poplar-ubuntu_20_04-3.1.0+6824-9c103dc348/enable.sh
+source /opt/gc/poplar/poplar_sdk-ubuntu_20_04-3.1.0+1205-58b501c780/popart-ubuntu_20_04-3.1.0+6824-9c103dc348/enable.sh
 mkdir -p /localdata/$USER/tmp
 export TF_POPLAR_FLAGS=--executable_cache_path=/localdata/$USER/tmp
 export POPTORCH_CACHE_DIR=/localdata/$USER/tmp
+source poptorch_venv/bin/activate
 # export POPLAR_LOG_LEVEL=INFO
 # export POPLIBS_LOG_LEVEL=INFO
